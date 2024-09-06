e86ab39c6bb7ed84b16e9e116a1369983dd44e3b
Jeffcar1993 <jphoenix.07@hotmail.com>
2024-09-05 21:07:51 -0500

    actualizacion de archivos y tarea 2

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function greet(name) {
+/*function greet(name) {
     return `Hello, ${name}!`;
   }
   
   console.log(greet("World"));
+*/
+  // tarea 2
+
+  function farewell(name) {
+    return `Goodbye, ${name}!`;
+}
+
+console.log(farewell("jeff"));
